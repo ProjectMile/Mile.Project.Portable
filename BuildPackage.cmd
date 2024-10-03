9460d29bd75dc233e487a223abea1ef1de0ea136
MouriNaruto <Kenji.Mouri@outlook.com>
2024-10-04 01:42:50 +0800

    Create Mile.Project.Portable project.

---
+++ BuildPackage.cmd
@@ -0,0 +1,33 @@
+@rem 
+@rem PROJECT:   Mouri Internal Library Essentials
+@rem FILE:      BuildPackage.cmd
+@rem PURPOSE:   Build Mile.Project.Portable Project script
+@rem 
+@rem LICENSE:   The MIT License
+@rem 
+@rem MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+@rem 
+
+@setlocal
+@echo off
+
+rem Change to the current folder.
+cd "%~dp0"
+
+rem Remove the output folder for a fresh compile.
+rd /s /q Output
+
+rem Initialize Visual Studio environment
+set VisualStudioInstallerFolder="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer"
+if %PROCESSOR_ARCHITECTURE%==x86 set VisualStudioInstallerFolder="%ProgramFiles%\Microsoft Visual Studio\Installer"
+pushd %VisualStudioInstallerFolder%
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set VisualStudioInstallDir=%%i
+)
+popd
+call "%VisualStudioInstallDir%\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+rem Build all targets
+MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m Mile.Project.Portable.proj
+
+@endlocal
